39e0ea661452ab6f6348c344a5009389ac0b5864
LokeshKumar-Dev <m.a.lokeshkumar@gmail.com>
2022-11-27 15:36:46 +0530

    first commmit

--- src/models/songD.js
+++
@@ -1,9 +0,0 @@
-module.exports = (connection, DataTypes) => {
-    const schema = {
-      name : DataTypes.STRING,
-      song : DataTypes.STRING,
-    }
-  
-    const SongDModel = connection.define('SongD', schema)
-    return  SongDModel;
-  }
