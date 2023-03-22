1c9d8e90d8d548f439b139bf860e5642c2ef6e8c
LokeshKumar-Dev <m.a.lokeshkumar@gmail.com>
2023-03-22 12:28:37 +0530

    postgres

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
+const pg = require("pg");
 const Sequelize = require("sequelize");
 
 const ArtistModel = require("./artist");
@@ -23,15 +24,27 @@ const {
 //   CLEARDB_DATABASE_URL,
 // } = process.env;
 
-
 const setupDatabase = () => {
-  const connection = MYSQL_URL
-    ? new Sequelize(MYSQL_URL)
-    : new Sequelize(MYSQLDATABASE, MYSQLUSER, MYSQLPASSWORD, {
-        host: MYSQLHOST,
-        port: MYSQLPORT,
-        dialect: "mysql",
-        logging: false,
+  // const params = url.parse(process.env.DATABASE_URL);
+  // const auth = params.auth.split(":");
+  console.log({
+    MYSQLDATABASE,
+    MYSQLUSER,
+    MYSQLPASSWORD,
+    MYSQLHOST,
+    MYSQLPORT,
+    MYSQL_URL,
+  })
+  const ssl = true; 
+  const connection = new Sequelize("musify_db_29", "loki", "ufHthCU4aXkh4WRRLQTTRxIyMvRWhmWX", {
+        host: "dpg-cgd8as02qv2aq5jd9q60-a",
+        port: 5432,
+        dialect: "postgres",
+        dialectOptions: {
+          ssl: ssl && typeof ssl === 'object' // use ssl instead of self
+              ? { rejectUnauthorized: false, ...self.ssl }
+              : { rejectUnauthorized: false },
+        },
       });
 
   const Artist = ArtistModel(connection, Sequelize);
@@ -44,6 +57,7 @@ const setupDatabase = () => {
   Song.belongsTo(Album, { as: "album" });
 
   connection.sync({ alter: true });
+  console.log("connected");
   return {
     Artist,
     Album,
